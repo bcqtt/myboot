808305848bbe3b06b68e622e533d536b2bb69385
laizhiwen <laizhiwen@evergrande.com>
2019-05-31 16:15:19 +0800

    Initial commit

---
+++ myboot-service/src/main/java/com/lz/myboot/MyBootApplication.java
@@ -0,0 +1,14 @@
+package com.lz.myboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class MyBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootApplication.class, args);
+
+	}
+}
